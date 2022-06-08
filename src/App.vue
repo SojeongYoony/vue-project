952da7e06c38ad81e145032a692b3fcf903dda12
SoJeongYoon <y940425@gmail.com>
2022-06-08 22:28:35 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+<!-- page도 component고 쪼꼬만 UI component다 마치 Class, vue/react 개념 
+    App.vue가 엄마 component 그리고 다른 애들은 App.vue의 자식들 -->
+    <h1>HELLO VUE</h1>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 지 멋대로 커스터마이징을 할 수 있음
+
+createApp(App).mount('#app')
